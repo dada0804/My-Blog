6ffa464629b789e767dd57767e4e4c21783f534d
dada0804 <62565567+dada0804@users.noreply.github.com>
2021-08-28 00:31:30 +0800

    fix contact_me.php

--- static/mail/contact_me.php
+++ static/mail/contact_me.php
@@ -1,28 +1,27 @@
-    <?php
-    // check if fields passed are empty
-    if(empty($_POST['name'])        ||
-    empty($_POST['email'])      ||
-    empty($_POST['message'])    ||
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-    echo "No arguments Provided!";
-    return false;
-   }
+{
+   echo "No arguments Provided!";
+   return false;
+}
 
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
 
-// create email body and send it
-$to = 'mayan1015@gmail.com'; // put your email
+// Create the email and send the message
+$to = 'mayan1015@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contact form submitted by:  $name";
-$email_body = "You have received a new message. \n\n".
-                  " Here are the details:\n \nName: $name \n ".
-                  "Email: $email_address\n Message \n $message";
-$headers = "From: noreply@myaddress.com\n";
-
-
+$email_body = "Hello, you have received a new message.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nTelephone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yanmablog.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+
+$result = mail($to,$email_subject,$email_body,$headers);
+die($result);
 ?>
