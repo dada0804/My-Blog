f8bc8fb0fa4cf95be0ce9d2caf5335b686e9aa93
dada0804 <62565567+dada0804@users.noreply.github.com>
2021-08-28 00:39:24 +0800

    update contact_me.php

--- static/js/contact_me.php
+++ static/js/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'mayan1015@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = '178102272@qq.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contact form submitted by:  $name";
 $email_body = "Hello, you have received a new message.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nTelephone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yanmablog.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
